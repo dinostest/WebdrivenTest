ee4cbc74a170d9eb5b8e406526418bccf86b6d8d
dinostest <liu980299@hotmail.com>
2014-10-03 17:02:01 +0930

    add test sample management
    add test sample management

---
+++ performance/migrations/0007_auto__add_fields.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Fields'
+        db.create_table(u'performance_fields', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('field_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('field_value', self.gf('django.db.models.fields.CharField')(max_length=2048)),
+            ('field_type', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('sample', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['performance.Sample'])),
+        ))
+        db.send_create_signal(u'performance', ['Fields'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Fields'
+        db.delete_table(u'performance_fields')
+
+
+    models = {
+        u'performance.application': {
+            'Meta': {'object_name': 'Application'},
+            'app_description': ('django.db.models.fields.TextField', [], {'max_length': '1024'}),
+            'app_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'performance.fields': {
+            'Meta': {'object_name': 'Fields'},
+            'field_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'field_type': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'field_value': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sample': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Sample']"})
+        },
+        u'performance.module': {
+            'Meta': {'object_name': 'Module'},
+            'application': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Application']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module_data': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'module_loop': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'module_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'module_ramp_up': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'module_target': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'module_testplan': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'module_threads': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        u'performance.sample': {
+            'Meta': {'object_name': 'Sample'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'priority': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'sample_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'scenario': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Scenario']"})
+        },
+        u'performance.scenario': {
+            'Meta': {'object_name': 'Scenario'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Module']"}),
+            'scenario_data': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'scenario_description': ('django.db.models.fields.TextField', [], {'default': "'Scenario description'", 'max_length': '1024'}),
+            'scenario_name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'performance.testreport': {
+            'Meta': {'object_name': 'TestReport'},
+            'URL': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
+            'func_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True'}),
+            'response_code': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'response_time': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'result': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True'}),
+            'running_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'sample_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'scenario': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Scenario']"}),
+            'target': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'ts_string': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'performance.testrun': {
+            'Meta': {'object_name': 'TestRun'},
+            'func_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True'}),
+            'module': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['performance.Module']"}),
+            'result': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True'}),
+            'target': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'ts_string': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        }
+    }
+
+    complete_apps = ['performance']
